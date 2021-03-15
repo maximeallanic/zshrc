57a3d47e7a200a22da5fef65550035f489173712
Maxime Allanic <maxime@allanic.me>
2021-03-15 16:21:48 +0100

    [ADD] install dependencies

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# Install packages
+apt install autojump zsh command-not-found
+
 # Antigen
 curl -L git.io/antigen > /usr/local/share/antigen.zsh
 
@@ -16,14 +19,13 @@ rm -rf /etc/vim
 ln -s `pwd`/vim /etc/vim
 
 # Install Zshrc
-rm -rf /etc/zshrc
-ln -s `pwd`/zshrc /etc/zshrc
+rm -rf /etc/zsh/zshrc
+ln -s `pwd`/zshrc /etc/zsh/zshrc
 
 touch ~/.zshrc
 
 # Install catfile
 ln -s `pwd`/cat-file.sh /usr/local/bin/catfile
-
 
 # Set Shell for Current User
 for user in $(eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1)
