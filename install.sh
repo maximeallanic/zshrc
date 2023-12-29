946e18784a405b026198f9b680e2fdcfc9f6b23a
Maxime Allanic <maxime@allanic.me>
2023-12-29 15:21:38 +0100

    fix install on debian and set optional ln

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 
 BASE_DIR=$(dirname "$0")
+USE_LINK="${USE_LINK:-NO}"
 
 apt update
 
@@ -19,23 +20,50 @@ chmod -R 777 /usr/local/share/nvm
 
 # Install Vimrc
 rm -rf /etc/vimrc
-ln -s $BASE_DIR/vimrc /etc/vimrc
+if [[ "$USE_LINK" == "YES" ]]; then
+	ln -s $BASE_DIR/vimrc /etc/vimrc
+else
+	cp $BASE_DIR/vimrc /etc/vimrc
+fi
 
 # Install Vim
 rm -rf /etc/vim
-ln -s $BASE_DIR/vim /etc/vim
+if [[ "$USE_LINK" == "YES" ]]; then
+	ln -s $BASE_DIR/vim /etc/vim
+else
+	cp -r $BASE_DIR/vim /etc/vim
+fi
 
 # Install Zshrc
 rm -rf /etc/zsh/zshrc
-ln -s $BASE_DIR/zshrc /etc/zsh/zshrc
+if [[ "$USE_LINK" == "YES" ]]; then
+	ln -s $BASE_DIR/zshrc /etc/zsh/zshrc
+else
+	cp $BASE_DIR/zshrc /etc/zsh/zshrc
+fi
 
 touch ~/.zshrc
 
 # Install catfile
-ln -s $BASE_DIR/cat-file.sh /usr/local/bin/catfile
+if [[ "$USE_LINK" == "YES" ]]; then
+	ln -s $BASE_DIR/cat-file.sh /usr/local/bin/catfile
+else
+	cp $BASE_DIR/cat-file.sh /usr/local/bin/catfile
+fi
+
+# Install antigen
+mkdir -p /usr/share/zsh-antigen
+if [[ "$USE_LINK" == "YES" ]]; then
+	ln -s $BASE_DIR/antigen.zsh /usr/share/zsh-antigen/antigen.zsh
+else
+	cp $BASE_DIR/antigen.zsh /usr/share/zsh-antigen/antigen.zsh
+fi
 
 UID_MIN=$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)
 UID_MAX=$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)
+
+touch /etc/zsh/zshrc.zwc
+chmod 777 /etc/zsh/zshrc.zwc
 
 # Set Shell for Current User
 echo $(awk -F: "\$3 >= $UID_MIN && \$3 <= $UID_MAX {print \$0}" /etc/passwd) | while read userLine 
@@ -46,7 +74,8 @@ do
     touch $home/.zshrc
 
     # Initialize new Shell
-    runuser -l $user -c 'zsh -c "source /etc/zsh/zshrc"'
+    runuser -l $user -c 'zsh -c "source /etc/zsh/zshrc"; nvm install 18'
+    chmod 777 /etc/zsh/zshrc.zwc
 done
 
 touch ~/.zshrc
@@ -54,6 +83,5 @@ chsh --shell `which zsh` root
 chown -R root:root /usr/local/share/zsh
 chmod -R go-w /usr/local/share/zsh
 
-zsh -c "source /etc/zsh/zshrc"
+zsh -c "source /etc/zsh/zshrc; nvm install 18"
 chmod 777 /etc/zsh/zshrc.zwc
-ls -la /etc/zsh/zshrc.zwc
